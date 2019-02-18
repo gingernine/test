28750de60788d6db7e98ac81a92617577a6ab8ec
gingernine <sy44895@gmail.com>
2019-02-18 20:45:41 +0900

    2019/02/18

--- Karatzas_Shreve_note/mendex.ps1
+++ Karatzas_Shreve_note/mendex.ps1
@@ -1,3 +1,9 @@
 platex Karatzas_Shreve_note.tex
 makeindex Karatzas_Shreve_note.idx
 pdfplatex Karatzas_Shreve_note.tex
+
+platex Karatzas_Shreve_note.tex
+platex Karatzas_Shreve_note.tex
+mendex -r -c -g -s dot.ist -p any Karatzas_Shreve_note.idx
+platex Karatzas_Shreve_note.tex
+platex --src-specials=math,par Karatzas_Shreve_note.tex
